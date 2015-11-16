be6b73128462a3008bf9bc9d47d0a76b7825b6f4
Erich Gamma <egamma@microsoft.com>
2015-11-16 17:12:05 +0100

    initial checkin

---
+++ README.md
@@ -0,0 +1,18 @@
+# Npm Script Running for Visual Studio Code
+
+This extension supports to run scripts defined in the `package.json` file.
+
+## Features
+- Run npm install.
+- Run a script (`npm run-script`) defined in the `package.json`. Pick a script from
+defined in the `scripts` section of the `package.json`.
+- Rerun the last npm script you have executed.
+
+## Using
+
+The commands defined by this extensions are in the `npm` category.
+
+![command palette](images/cmds.png)
+
+With the setting `npm.runInTerminal` you can configure whether the command is run
+in terminal window or whether the output form the command should show up in the `Output` window.
---
+++ src/main.ts
@@ -0,0 +1,113 @@
+import * as cp from 'child_process';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import { window, commands, workspace, OutputChannel, ExtensionContext, ViewColumn} from 'vscode';
+import { runInTerminal } from 'run-in-terminal';
+
+interface Script {
+	label: string;
+	execute(): void;
+	description: string;
+}
+
+let outputChannel: OutputChannel;
+let lastScript: Script = null;
+
+export function activate(context: ExtensionContext) {
+	registerCommands(context);
+	outputChannel = window.createOutputChannel('npm');
+	context.subscriptions.push(outputChannel);
+}
+
+function registerCommands(context: ExtensionContext) {
+	let c1 = commands.registerCommand('npm-script.showOutput', showNpmOutput);
+	let c2 = commands.registerCommand('npm-script.install', runNpmInstall);
+	let c3 = commands.registerCommand('npm-script.run', runNpmScript);
+	let c4 = commands.registerCommand('npm-script.rerun-last-script', rerunLastScript);
+	context.subscriptions.push(c1, c2, c3, c4);
+}
+
+function runNpmInstall() {
+	runNpmCommand(['install']);
+}
+
+function showNpmOutput(): void {
+	outputChannel.show(ViewColumn.Three);
+}
+
+function runNpmScript(): void {
+	let scripts = readScripts();
+	if (!scripts) {
+		return;
+	}
+	let scriptList: Script[] = [];	
+	Object.keys(scripts).forEach(key => {
+		scriptList.push({
+			label: `${key}`,
+			description: `${scripts[key]}`,
+			execute() {
+				lastScript = this;
+				runNpmCommand(['run-script', key]);
+			}
+		});
+	});
+
+	window.showQuickPick(scriptList).then(script => {
+		if (script) {
+			return script.execute();
+		}
+	});
+};
+
+function rerunLastScript(): void {
+	if (lastScript) {
+		lastScript.execute();
+	} else {
+		runNpmScript();
+	}
+}
+
+function readScripts(): any {
+	let fileName = path.join(workspace.rootPath, 'package.json');
+	try {
+		var contents = fs.readFileSync(fileName).toString();
+		var json = JSON.parse(contents);
+		if (json.scripts) {
+			return json.scripts;
+		}
+		window.showInformationMessage('No scripts are defined in \'package.json\'');
+		return undefined;
+	} catch(e) {
+		window.showInformationMessage('Cannot read \'package.json\'');
+		return undefined;
+	}
+}
+
+function runNpmCommand(args: string[]): void {
+	if (useTerminal()) {
+		runCommandInTerminal(args);
+	} else {
+		runCommandInOutputWindow(args);
+	}
+}
+
+function runCommandInOutputWindow(args: string[]) {
+	let cmd = 'npm ' + args.join(' ');
+	let p = cp.exec(cmd, { cwd: workspace.rootPath, env: process.env });
+	p.stderr.on('data', (data: string) => {
+		outputChannel.append(data);
+	});
+	p.stdout.on('data', (data: string) => {
+		outputChannel.append(data);
+	});
+	showNpmOutput();
+}
+
+function runCommandInTerminal(args: string[]): void {
+	runInTerminal('npm', args, { cwd: workspace.rootPath, env: process.env });
+}
+
+function useTerminal() {
+	return workspace.getConfiguration('npm')['runInTerminal'];
+}
